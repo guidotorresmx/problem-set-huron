17253799f1086c1578ba33500dee0186735548f6
Marco Guido <guido-torres@hotmail.com>
2022-09-19 03:56:57 -0400

    added print function

--- solution to problem 1/python/src/utils.py
+++ solution to problem 1/python/src/utils.py
@@ -1,4 +1,11 @@
 from fractions import Fraction
 
-def fractional_formater(probs):
-    probs = {k: Fraction(v).limit_denominator() for k, v in probs.items()}
+def format_to_fractional(probs):
+    return {k: Fraction(v).limit_denominator() for k, v in probs.items()}
+
+def print_probs(probs):
+    for key, prob in probs.items():
+        if type(prob) is Fraction:
+            print(f"{key} = {prob}")
+        else: 
+            print(f"{key} = {prob:.3f}")
