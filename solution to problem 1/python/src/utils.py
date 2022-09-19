99d1d13a6ab75e41cc157490e8bf0a439b023722
Marco Guido <guido-torres@hotmail.com>
2022-09-19 12:24:48 -0400

    added new utils function for dict normalization

--- solution to problem 1/python/src/utils.py
+++ solution to problem 1/python/src/utils.py
@@ -7,5 +7,22 @@ def print_probs(probs):
     for key, prob in probs.items():
         if type(prob) is Fraction:
             print(f"    {key} = {prob}")
-        else: 
+        else:
             print(f"    {key} = {prob:.2f}")
+
+def normalize_probs(probs: Dict) -> Dict:
+    '''
+    modifies values from dict, so the sum of its values is 1
+        params:
+            probs = {
+                1: 4,
+                2: 3,
+                3: 3,
+                }
+        returns:
+            probs = {
+                1: .4,
+                2: .3,
+                3: .3,
+                }
+    '''
