833047a13bc8c5af6499924466639c095dbbfafc
Marco Guido <guido-torres@hotmail.com>
2022-09-19 03:15:24 -0400

    added main function for direct execution

--- solution to problem 1/python/src/programmatic.py
+++ solution to problem 1/python/src/programmatic.py
@@ -1,48 +1,52 @@
 import itertools
 from fractions import Fraction
+from typing import Dict, Set
 
-def get_probabilities_programmatically(s: set, r: int) -> dict:
-    """
-        Recives a set (s) of numbers and a sampling number (r), from which, the
-        function returns the probability of of picking each element as the minimum
-        of each set
-            params:
-                s: set of integers
-                r: number of sampled elements
-            return:
-                probs: dict of probabilities of minimum of subsets
-
-        e.g.:
-            params:
-                s = {49, 8, 48, 15, 47, 4, 16, 23, 43, 44, 42, 45, 46}.
-                r = 6
-            returns:
-                probs = {
-                    4: .41,
-                    8: .22,
-                    15 .16,
-                }
-    """
+def get_probs_programmatically(s: Set, r: int) -> Dict:
+    '''
+    Recives a set (s) of numbers and a sampling number (r), from which, the
+    function returns the probability of of picking each element as the minimum
+    of each set
+        params:
+            s: set of integers
+            r: number of sampled elements
+        return:
+            probs: dict of probabilities of minimum of subsets
+    e.g.:
+        params:
+            s = {49, 8, 48, 15, 47, 4, 16, 23, 43, 44, 42, 45, 46}.
+            r = 6
+        returns:
+            probs = {
+                4: .41,
+                8: .22,
+                15 .16,
+        }
+    '''
 
     if not s:
         raise ValueError("please specify a set")
     if not r:
         raise ValueError("please specify a sampling number")
 
-    s = list(range(1,lenS+1))
     n = len(s)
-    lenS = len(s)
-    sIndexes = list(range(1,n+1))
+    sIndexes = list(range(1, n + 1))
 
     combinations = list(itertools.combinations(sIndexes, r))
-    probs = { i:0 for i in range(1,n+1)}
+    probs = {i: 0 for i in range(1, n + 1)}
 
     for combination in combinations:
         probs[min(combination)] += 1
 
     total = sum(probs.values())
-    for sIndex, _  in probs.items():
+    for sIndex, _ in probs.items():
         probs[sIndex] /= total
 
     return probs
 
+
+if __name__ == "__main__":
+    s = sorted([49, 8, 48, 15, 47, 4, 16, 23, 43, 44, 42, 45, 46])
+    r = 6
+    probs = get_probs_programmatically(s, r)
+    print(probs)
